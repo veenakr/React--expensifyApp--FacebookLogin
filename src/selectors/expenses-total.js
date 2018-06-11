7ac35642afcd2d471db6c2fa79a5b0aeb73a503a
Veena KR <veenakr@Veenas-MacBook-Air.local>
2018-06-11 22:51:58 +0530

    Add expense summary to dashboard

--- src/selectors/expenses-total.js
+++ src/selectors/expenses-total.js
@@ -1,6 +1,5 @@
-export default (expenses) => {
-    return expenses
-        .map((expense) => expense.amount)
+export default(expenses) => {
+    return expenses.
+        map((expense) => expense.amount)
         .reduce((sum, value) => sum + value, 0);
-  };
-  
+};
