b477197188fc912d1dd89488f8b03bae79edbc25
Veena KR <veenakr@Veenas-MacBook-Air.local>
2018-06-18 23:58:46 +0530

    Add private routes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,7 +13,9 @@ const config = {
 
   const database = firebase.database();
 
-  export { firebase, database as default };
+  const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+  export { firebase, googleAuthProvider, database as default };
 
 //   //child_removed
 
